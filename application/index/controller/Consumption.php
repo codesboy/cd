3b5a3bf6a2be2ed525ba80907e36030d5e59c983
Codesboy <565195693@qq.com>
2016-10-23 22:42:52 +0800

    datagrid加载消费详情子网络

---
+++ application/index/controller/Consumption.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @authors Rehack (565195693@qq.com)
+ * @link    http://rehack.cn
+ * @date    2016-10-23 21:49:10
+ * @version $Id$
+ */
+
+namespace app\index\controller;
+use app\index\model\Consumption as cpmodel;
+
+class Consumption extends Base{
+    // 获取消费详情
+    public function getcpdetail(){
+        if(Request()->isGet()){
+            $uid=input('uid');
+            $cp=cpmodel::where('uid',$uid)
+            ->join('wangdian_zixun w','w.id=wdzx_id')
+            ->join('qiantai_zixun q','q.id=qtzx_id')
+            ->join('doctors d','d.id=doctor_id')
+            ->join('disease dis','dis.id=disease_id')
+            ->field('w.name wd,q.name qt,doctor,disease_name,money,ill_desc,jz_time')
+            ->select();
+            return json($cp);
+        }
+        // dump($cp);
+    }
+}
--- application/index/model/Consumption.php
+++ application/index/model/Consumption.php
@@ -7,14 +7,14 @@ class Consumption extends Model{
     protected $type       = [
         'jz_time'    =>  'timestamp'
     ];
-    /*
+
     // 开启自动写入时间戳protected $autoWriteTimestamp = true;
 
     // 定义自动完成的属性protected $insert = ['status' => 1];
 
     // 定义关联方法
     public function usersinfo(){
-        return $this->belongsTo('UsersInfo--');
-    }*/
+        return $this->belongsTo('UsersInfo');
+    }
 
 }
