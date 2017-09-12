cc63fb5d88ab577508504625958fce11ca4e7f37
codesboy <565195693@qq.com>
2017-09-12 18:02:08 +0800

    update

--- application/upload/controller/Index.php
+++ application/upload/controller/Index.php
@@ -10,59 +10,79 @@ class Index extends Controller{
         return $this->fetch();
     }
 
-    public function upload(Request $request){
-        // 获取表单上传文件 例如上传了001.jpg
+    private function upload(){
+        // 获取表单上传文件
         $file = request()->file('file');
         $num=request()->param('num');
-        // dump($file);die;
-        // dump($num);die;
-        // dump($error = $_FILES);
-        if(empty($file) && !$num){
-            throw new UploadException();
+
+        // 针对getimagesize(): Read error!异常
+        try {
+            // 上传文件验证
+            $result = $this->validate(['file' => $file], ['file'=>'require|image|fileSize:6291456'],['file.require' => '请选择上传文件', 'file.image' => '非法图像文件','file.fileSize' => '图片文件大小不能超过6M']);//  20756
+        } catch (\Exception $e) {
+            throw new UploadException([
+                'code'=>0,
+                'msg'=>$e->getMessage()
+            ]);
         }
 
-        $result = $this->validate(['file' => $file], ['file'=>'require|image|fileSize:6291456'],['file.require' => '请选择上传文件', 'file.image' => '非法图像文件','file.fileSize' => '图片文件大小不能超过6M']);//  20756
+        // 验证不通过
         if(true !== $result){
             throw new UploadException([
                 'msg'=>$this->error($result)
             ]);
         }
 
-
         $image = Image::open($file);
-        // 缩略图处理
+        // 图片压缩处理
         $image->thumb(800, 800, Image::THUMB_CENTER);
 
+        // 唯一文件名 微妙md5加密
+        $saveName = md5(uniqid(microtime(true),true)) . '.' . $image->type();
         // 保存图片
-        $saveName = md5($request->time()) . '.' . $image->type();
         $image->save(ROOT_PATH . 'public/uploads/' . $saveName);
-        dump($image);
-        // $image->save(ROOT_PATH . 'public' . DS . 'uploads'.$saveName);
+        if(empty($image)){
+            return;
+        }
+
+        $arr=[];
+        $arr['sign_num']=$num;
+        $arr['img_url']=$saveName;
+        return $arr;
+
+    }
 
 
-        /*// 移动到框架应用根目录/public/uploads/ 目录下
-        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
-        if($info){
-            $data=[
-                'sign_num'=>$num,
-                'img_url'=>$info->getSaveName()
-            ];
+    public function saveUploadFile(){
+        $data=$this->upload();
+        if(!empty($data)){
             $res=UploadsModel::create($data);
             if(!$res){
-                return "上传失败";
+                throw new UploadException([
+                    'msg'=>'上传失败',
+                    'code'=>400,
+                    'errorCode'=>6000
+                ]);
+            }else{
+                throw new UploadException([
+                    'msg'=>'上传成功',
+                    'code'=>200,
+                    'errorCode'=>0
+                ]);
             }
-
-            // return "上传成功";
-            return $info->getRealPath();
-        }else{
-            // 上传失败获取错误信息
-            echo $file->getError();
-        }*/
+        }
     }
 
     public function getUploadsData()
     {
         $data=UploadsModel::where('sign_num','>',0)->select();
-        return json($data);
+        return $this->fetch();
+        // return json($data);
     }
+
+    public function test(){
+        var_dump(getimagesize('C:\Users\admin\Desktop\2.jpg'));
+    }
+
+
 }
